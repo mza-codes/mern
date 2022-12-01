cd66a4874308b14eac02ed642ad8866f3817aa8e
mza-codes <muzofficial90@gmail.com>
2022-12-01 08:43:16 +0000

    Added bunyan,Updated Status codes

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const errorHandler = require('./middlewares/errorHandler');
 const helmet = require('helmet');
+const bunyan = require('bunyan');
 
 // Database Connection
 const connectDB = async () => {
@@ -14,9 +15,9 @@ const connectDB = async () => {
         // disabled as it clearly shows error: <> MongoParseError: options usecreateindex, usefindandmodify are not supported </>
         useUnifiedTopology: true,
         useNewUrlParser: true,
-    }).then(() => console.log('DB Connection Success !'))
+    }).then(() => log.info('DB Connection Success !'))
         .catch((err) => {
-            console.log('DB Connection Failed', err);
+            log.error('DB Connection Failed', err);
             process.exit(0);
         });
 };
@@ -28,6 +29,7 @@ app.use(cors());
 app.use(express.json());
 app.use(helmet());
 
+const log = bunyan.createLogger({name: 'express-backend'});
 
 // Routes
 app.use('/api/v1/auth', require('./routes/auth'));
@@ -37,7 +39,7 @@ app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     connectDB();
-    console.log(`Server Started On PORT: ${PORT}`);
+    log.info(`Server Started On PORT: ${PORT}`);
 });
 
 
