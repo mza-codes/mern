8b11e4dc46cd61e1312b4455d8002fbb5c37febf
mza-codes <muzofficial90@gmail.com>
2022-12-01 11:06:56 +0000

    Functional Changes,Under Development

--- middlewares/authorizeUser.js
+++ middlewares/authorizeUser.js
@@ -2,12 +2,29 @@ require('dotenv').config();
 
 const asyncHandler = require("./asyncHandler");
 const jwt = require("jsonwebtoken");
+const { createAccessToken } = require('../controllers/authControllers');
 
 exports.checkAuthorization = asyncHandler(async (req, res, next) => {
     console.log("Checking Authorization");
     const token = req.headers?.authorization?.split(" ")[1];
-    if (!token) { return res.status(401).json({ success: false, message: "User token not found" }); };
-    let data = jwt.verify(token,process.env.JWT_KEY ?? "m$auth");
+
+    if (!token) {
+        return res.status(401).json({ success: false, message: "User token not found" });
+    };
+
+    let data = jwt.verify(token, process.env.JWT_KEY ?? "m$auth");
+    req.userId = data.userId;
+    next();
+});
+
+exports.refreshToken = asyncHandler(async (req, res, next) => {
+    const token = req.headers?.authorization?.split(" ")[1];
+
+    if (!token) {
+        return res.status(401).json({ success: false, message: "User token not found" });
+    };
+
+    let data = jwt.verify(token, process.env.JWT_KEY ?? "m$authRefresh");
     req.userId = data.userId;
     next();
 });
--- routes/auth.js
+++ routes/auth.js
@@ -6,6 +6,9 @@ const jwtAuth = require('../middlewares/authorizeUser');
 // @route - /api/v1/auth/
 router.route('/register').post(authControllers.createAuth);
 router.route('/login').post(authControllers.auth);
+router.get('/logout',jwtAuth.checkAuthorization, authControllers.logout);
+router.get('/refreshToken',jwtAuth.refreshToken, authControllers.logout);
+
 router.route('/test2').get(jwtAuth.checkAuthorization, authControllers.updateAuth);
 router.get('/test', authControllers.updateAuth);
 
